6f06abbafc11d1fe5b566183c6e593e9add1c1d9
capricious-slingshot <ephemere@protonmail.com>
2021-12-27 23:38:40 -0800

    adding Redux

--- components/About.js
+++ components/About.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import Mission from './Mission';
-import { FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { ScrollView, Text, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+  return {
+    partners: state.partners
+  };
+};
 
 class About extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      partners: PARTNERS
-    };
-  }
   static navigationOptions = {
     title: 'About'
   }
@@ -19,7 +19,7 @@ class About extends Component {
         <ListItem
           title={item.name}
           subtitle={item.description}
-          leftAvatar={{ source: require('../assets/images/bootstrap-logo.png') }}
+          leftAvatar={{ source: {uri: baseUrl + item.image} }}
         />
       );
     };
@@ -32,7 +32,7 @@ class About extends Component {
           <Card.Title>Community Partners</Card.Title>
           <Card.Divider />
           <FlatList
-            data={this.state.partners}
+            data={this.props.partners.partners}
             renderItem={renderPartner}
             keyExtractor={item => item.id.toString()}
           />
@@ -42,4 +42,4 @@ class About extends Component {
   }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
