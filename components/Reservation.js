8f3a2038e17e50f6ac5c8004cb9553b623f9d67f
capricious-slingshot <ephemere@protonmail.com>
2022-01-03 20:23:44 -0800

    adding in notification confirmatio for campsite confirmation

--- components/Reservation.js
+++ components/Reservation.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, Animated, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
   constructor(props) {
@@ -49,7 +50,10 @@ class Reservation extends Component {
         },
         {
           text: 'OK',
-          onPress: () => console.log('Ok pressed'),
+          onPress: () => {
+            this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+            this.resetForm();
+          }
         }
       ],
       { cancelable: false }
@@ -64,6 +68,34 @@ class Reservation extends Component {
       date: new Date(),
       showCalendar: false,
     });
+  }
+
+  async presentLocalNotification(date) {
+    function sendNotification() {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true
+        })
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Your Campsite Reservation Search',
+          body: `Search for ${date} requested`
+        },
+        trigger: null
+      });
+    }
+
+    let permissions = await Notifications.getPermissionsAsync();
+
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+
+    if (permissions.granted) {
+      sendNotification();
+    }
   }
 
   render() {
