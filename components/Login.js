6a43f6f5ddb2318d469cc2cdf933fd1835dab056
capricious-slingshot <ephemere@protonmail.com>
2022-01-04 14:37:13 -0800

    adding register authentication form view to login tabbed navigation

--- components/Login.js
+++ components/Login.js
@@ -119,41 +119,124 @@ class LoginTab extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    margin: 20
-  },
-  formIcon: {
-    marginRight: 10
-  },
-  formInput: {
-    padding: 10
-  },
-  formCheckbox: {
-    margin: 10,
-    backgroundColor: null
-  },
-  formButton: {
-    margin: 40
-  }
-});
+class RegisterTab extends Component {
+  constructor(props) {
+    super(props);
 
-class RegisterTab extends Component { 
+    this.state = {
+      username: '',
+      password: '',
+      firstname: '',
+      lastname: '',
+      email: '',
+      remember: false,
+      imageUrl: `${baseUrl} images/logo.png`
+    };
+    }
+
   static navigationOptions = {
     title: 'Register',
     tabBarIcon: ({tintColor}) => (
       <Icon
-          name='user-plus'
-          type='font-awesome'
-          iconStyle={{color: tintColor}}
+        name='user-plus'
+        type='font-awesome'
+        iconStyle={{color: tintColor}}
       />
     )
   }
 
+  handleRegister() {
+    console.log(JSON.stringify(this.state));
+    if (this.state.remember) {
+      SecureStore.setItemAsync('userinfo', JSON.stringify(
+        {username: this.state.username, password: this.state.password}))
+        .catch(error => console.log('Could not save user info', error));
+    } else {
+      SecureStore.deleteItemAsync('userinfo').catch(
+        error => console.log('Could not delete user info', error)
+      );
+    }
+  }
+
   render() {
     return (
-      <ScrollView></ScrollView>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.imageContainer}>
+            <Image
+              source={{uri: this.state.imageUrl}}
+              loadingIndicatorSource={require('../assets/images/logo.png')}
+              style={styles.image}
+            />
+            <Button
+              title='Camera'
+              onPress={this.getImageFromCamera}
+            />
+          </View>
+          <Input
+            placeholder='Username'
+            leftIcon={{type: 'font-awesome', name: 'user-o'}}
+            onChangeText={username => this.setState({username})}
+            value={this.state.username}
+            containerStyle={styles.formInput}
+            leftIconContainerStyle={styles.formIcon}
+          />
+          <Input
+            placeholder='Password'
+            leftIcon={{type: 'font-awesome', name: 'key'}}
+            onChangeText={password => this.setState({password})}
+            value={this.state.password}
+            containerStyle={styles.formInput}
+            leftIconContainerStyle={styles.formIcon}
+          />
+          <Input
+            placeholder='First Name'
+            leftIcon={{type: 'font-awesome', name: 'user-o'}}
+            onChangeText={firstname => this.setState({firstname})}
+            value={this.state.firstname}
+            containerStyle={styles.formInput}
+            leftIconContainerStyle={styles.formIcon}
+          />
+          <Input
+            placeholder='Last Name'
+            leftIcon={{type: 'font-awesome', name: 'user-o'}}
+            onChangeText={lastname => this.setState({lastname})}
+            value={this.state.lastname}
+            containerStyle={styles.formInput}
+            leftIconContainerStyle={styles.formIcon}
+          />
+          <Input
+            placeholder='Email'
+            leftIcon={{type: 'font-awesome', name: 'envelope-o'}}
+            onChangeText={email => this.setState({email})}
+            value={this.state.email}
+            containerStyle={styles.formInput}
+            leftIconContainerStyle={styles.formIcon}
+          />
+          <CheckBox
+            title='Remember Me'
+            center
+            checked={this.state.remember}
+            onPress={() => this.setState({remember: !this.state.remember})}
+            containerStyle={styles.formCheckbox}
+          />
+          <View style={styles.formButton}>
+            <Button
+              onPress={() => this.handleRegister()}
+              title='Register'
+              icon={
+                <Icon
+                  name='user-plus'
+                  type='font-awesome'
+                  color='#fff'
+                  iconStyle={{marginRight: 10}}
+                />
+              }
+              buttonStyle={{backgroundColor: '#5637DD'}}
+            />
+          </View>
+                </View>
+      </ScrollView>
     )
   }
 }
@@ -173,5 +256,38 @@ const Login = createBottomTabNavigator(
     }
   }
 );
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        margin: 10
+    },
+    formIcon: {
+        marginRight: 10
+    },
+    formInput: {
+        padding: 8
+    },
+    formCheckbox: {
+        margin: 8,
+        backgroundColor: null
+    },
+    formButton: {
+        margin: 20,
+        marginRight: 40,
+        marginLeft: 40
+    },
+    imageContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        margin: 10
+    },
+    image: {
+        width: 60,
+        height: 60
+    }
+});
 
 export default Login;
