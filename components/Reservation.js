2a987200fd9b8f5cb685dcf19866f0aef99a520a
capricious-slingshot <ephemere@protonmail.com>
2022-01-03 17:54:28 -0800

    workshop: task 2

--- components/Reservation.js
+++ components/Reservation.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Animated, StyleSheet, Picker, Switch, Button, Modal } from 'react-native';
+import { Text, View, Animated, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 class Reservation extends Component {
@@ -10,7 +10,6 @@ class Reservation extends Component {
       hikeIn: false,
       date: new Date(),
       showCalendar: false,
-      showModal: false,
       scaleValue: new Animated.Value(0)
     };
   }
@@ -35,13 +34,27 @@ class Reservation extends Component {
     title: 'Reserve Campsite'
   }
 
-  toggleModal() {
-    this.setState({showModal: !this.state.showModal});
-  }
-
   handleReservation() {
     console.log(JSON.stringify(this.state));
-    this.toggleModal();
+    Alert.alert(
+      'Begin Search?',
+      `Number of Campers: ${this.state.campers} \n 
+       Hike-In?: ${this.state.hikeIn} \n
+       Date:${this.state.date}`,
+      [
+        {
+          text: 'Cancel',
+          onPress: () => this.resetForm(),
+          style: 'cancel'
+        },
+        {
+          text: 'OK',
+          onPress: () => console.log('Ok pressed'),
+        }
+      ],
+      { cancelable: false }
+    )
+    // this.resetForm(); here? of on button?
   }
 
   resetForm() {
@@ -50,7 +63,6 @@ class Reservation extends Component {
       hikeIn: false,
       date: new Date(),
       showCalendar: false,
-      showModal: false
     });
   }
 
@@ -110,34 +122,6 @@ class Reservation extends Component {
             color='#5637DD'
             accessibilityLabel='Tap me to search for available campsites to reserve' />
         </View>
-
-        <Modal
-          animationType={'slide'}
-          transparent={false}
-          visible={this.state.showModal}
-          onRequestClose={() => this.toggleModal()}
-        >
-          <View style={styles.modal}>
-            <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-            <Text style={styles.modalText}>
-              Number of Campers: {this.state.campers}
-            </Text>
-            <Text style={styles.modalText}>
-              Hike-In?: {this.state.hikeIn ? 'Yes' : 'No'}
-            </Text>
-            <Text style={styles.modalText}>
-              Date: {this.state.date.toLocaleDateString('en-US')}
-            </Text>
-            <Button
-              onPress={() => {
-                this.toggleModal();
-                this.resetForm();
-              }}
-              color='#5637DD'
-              title='Close'
-            />
-          </View>
-        </Modal>
       </Animated.View>
     );
   }
@@ -157,22 +141,6 @@ const styles = StyleSheet.create({
   },
   formItem: {
     flex: 1
-  },
-  modal: {
-    justifyContent: 'center',
-    margin: 20
-  },
-  modalTitle: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    backgroundColor: '#5637DD',
-    textAlign: 'center',
-    color: '#fff',
-    marginBottom: 20
-  },
-  modalText: {
-    fontSize: 18,
-    margin: 10
   }
 });
 
